33010469afe83266783ef9ba2b30e21d8c03ccf7
Joshwa Facistol <fastline2123@gmail.com>
2021-05-23 01:19:20 +0800

    update header and pages

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+	return (
+		<Fragment>
+			<Navbar bg='light' expand='lg'>
+				<Navbar.Brand href='/'>
+					{process.env.REACT_APP_TITLE}
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='ml-auto'>
+						<Nav.Link exact as={NavLink} to='/'>
+							Home
+						</Nav.Link>
+						<Nav.Link as={NavLink} to='/login'>
+							Login
+						</Nav.Link>
+						<Nav.Link as={NavLink} to='/register'>
+							Register
+						</Nav.Link>
+						<Nav.Link as={NavLink} to='/dashboard'>
+							Dashboard
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</Fragment>
+	);
+};
+
+export default Header;
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+	user: null
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
