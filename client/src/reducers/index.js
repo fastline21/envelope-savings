b12078b39146dcde6280f37fc2fe8d9b9eff5448
Joshwa Facistol <fastline2123@gmail.com>
2021-05-26 00:04:10 +0800

    remove UI

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
-import alertReducer from './alertReducer';
-
-export default combineReducers({
-	userState: userReducer,
-	alertState: alertReducer,
-});
