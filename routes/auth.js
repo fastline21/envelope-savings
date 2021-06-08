3969d3b0fd8df01e96bb1a38af141b2728da06ce
Joshwa Facistol <fastline2123@gmail.com>
2021-06-09 03:15:54 +0800

    checked auth complete

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,6 @@ const auth = require("./../middleware/auth");
 
 // Login user
 router.post("/", async (req, res) => {
-	console.log(req.body)
 	const { email, password } = req.body;
 	if (!email || !password) {
 		return res
