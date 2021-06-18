7b238b79eb3fae8703c9c61c3fe220a45d32876f
Joshwa Facistol <fastline2123@gmail.com>
2021-06-18 21:12:55 +0800

    Added Container in About

--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const About = () => {
-	return <h1>About</h1>;
+	return (
+		<Container>
+			<h1>About</h1>
+		</Container>
+	);
 };
 
 export default About;
